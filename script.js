1afb6db261d0db9317bbef0486dcdf36244fe314
ALI-KASBATI <alikasbati78@gmail.com>
2023-03-01 03:09:13 +0500

    Work Complete

---
+++ script.js
@@ -0,0 +1,82 @@
+let percentage = prompt('Enter Your Percentage');
+
+if (percentage>100){
+    alert ('Invalid Percentage')
+
+    console.log ('Invalid Percentage')
+}
+
+else if (percentage>=95)
+{
+    alert ('Congratulations! You have got A++ Grade')
+
+    console.log ('Congratulations! You have got A++ Grade')
+}
+
+else if (percentage>=90)
+{
+    alert ('Congratulations! You have got A+ Grade')
+
+    console.log ('Congratulations! You have got A+ Grade')
+} 
+
+else if (percentage>=85)
+{
+    alert ('Congratulations! You have got A Grade')
+
+    console.log ('Congratulations! You have got A Grade')
+}
+
+else if (percentage>=80)
+{
+    alert ('Congratulations! You have got B++ Grade')
+
+    console.log ('Congratulations! You have got B++ Grade')
+}
+
+else if (percentage>=75)
+{
+    alert (' You have got B+ Grade')
+
+    console.log (' You have got B+ Grade')
+}
+
+else if (percentage>=70)
+{
+    alert (' You have got B Grade')
+
+    console.log (' You have got B Grade')
+}
+
+else if (percentage>=60)
+{
+    alert (' You have got C Grade')
+
+    console.log (' You have got C Grade')
+}
+
+else if (percentage>=50)
+{
+    alert (' You have got D Grade')
+
+    console.log (' You have got D Grade')
+}
+
+else if (percentage>=40)
+{
+    alert (' You have got E Grade')
+
+    console.log (' You have got E Grade')
+}
+
+else if (percentage>=0) {
+    alert ('Sorry! You Are Failed')
+
+    console.log ('Sorry! You Are Failed')
+}
+
+else {
+    alert ('Invalid Percentage')
+
+    console.log ('Invalid Percentage')
+}
